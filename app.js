65f33f3277ac76d18bdb956798acc5edd747a2f6
张卓 <mycinbrin@gmail.com>
2020-01-28 19:53:01 +0800

    refactor app.js

--- app.js
+++ app.js
@@ -1,93 +1,104 @@
-document.getElementById("title").innerHTML +=
-  "（最后更新于 " + LAST_UPDATE + "）";
+var map;
+var disProvince;
+var disChongqing;
+var layer;
+var verbose = false;
 
-var map = new AMap.Map("container", {
-  zoom: window.screen.width <= 600 ? 3 : 4,
-  center: [104.5, 38.5],
-  pitch: 0,
-  viewMode: "3D"
-});
+function initVirusMap() {
+  document.getElementById("title").innerHTML +=
+    "（最后更新于 " + LAST_UPDATE + "）";
 
-// 创建省份图层
-// 排除重庆(500000)
-var all_provinces = [
-  "110000",
-  "120000",
-  "130000",
-  "140000",
-  "150000",
-  "210000",
-  "220000",
-  "230000",
-  "310000",
-  "320000",
-  "330000",
-  "340000",
-  "350000",
-  "360000",
-  "370000",
-  "410000",
-  "420000",
-  "430000",
-  "440000",
-  "450000",
-  "460000",
-  "510000",
-  "520000",
-  "530000",
-  "540000",
-  "610000",
-  "620000",
-  "630000",
-  "640000",
-  "650000"
-];
-var disProvince = new AMap.DistrictLayer.Province({
-  zIndex: 12,
-  adcode: all_provinces,
-  depth: 1,
-  styles: {
-    fill: function(properties) {
-      return DATA[properties.adcode]["color"];
-    },
-    "province-stroke": "black",
-    "city-stroke": "white", // 中国地级市边界
-    "county-stroke": "rgba(255,255,255,0.5)" // 中国区县边界
+  map = new AMap.Map("container", {
+    zoom: window.screen.width <= 600 ? 3 : 4,
+    center: [104.5, 38.5],
+    pitch: 0,
+    viewMode: "3D"
+  });
+
+  // 创建省份图层
+  // 排除重庆(500000)
+  var all_provinces = [
+    "110000",
+    "120000",
+    "130000",
+    "140000",
+    "150000",
+    "210000",
+    "220000",
+    "230000",
+    "310000",
+    "320000",
+    "330000",
+    "340000",
+    "350000",
+    "360000",
+    "370000",
+    "410000",
+    "420000",
+    "430000",
+    "440000",
+    "450000",
+    "460000",
+    "510000",
+    "520000",
+    "530000",
+    "540000",
+    "610000",
+    "620000",
+    "630000",
+    "640000",
+    "650000"
+  ];
+  disProvince = new AMap.DistrictLayer.Province({
+    zIndex: 12,
+    adcode: all_provinces,
+    depth: 1,
+    styles: {
+      fill: function(properties) {
+        return DATA[properties.adcode]["color"];
+      },
+      "province-stroke": "black",
+      "city-stroke": "white", // 中国地级市边界
+      "county-stroke": "rgba(255,255,255,0.5)" // 中国区县边界
+    }
+  });
+
+  disProvince.setMap(map);
+
+  disChongqing = new AMap.DistrictLayer.Province({
+    zIndex: 12,
+    adcode: ["500000"],
+    depth: 2,
+    styles: {
+      fill: function(properties) {
+        return DATA[properties.adcode]["color"];
+      },
+      "province-stroke": "black",
+      "city-stroke": "white", // 中国地级市边界
+      "county-stroke": "rgba(255,255,255,0.5)" // 中国区县边界
+    }
+  });
+
+  disChongqing.setMap(map);
+
+  //initClickHandler();
+}
+
+function clickHander(ev) {
+  if (verbose) {
+    console.log("ev", ev);
   }
-});
-
-disProvince.setMap(map);
-
-var disChongqing = new AMap.DistrictLayer.Province({
-  zIndex: 12,
-  adcode: ["500000"],
-  depth: 2,
-  styles: {
-    fill: function(properties) {
-      return DATA[properties.adcode]["color"];
-    },
-    "province-stroke": "black",
-    "city-stroke": "white", // 中国地级市边界
-    "county-stroke": "rgba(255,255,255,0.5)" // 中国区县边界
-  }
-});
-
-disChongqing.setMap(map);
-
-var layer = new AMap.LabelsLayer({
-  fitView: true,
-  map: map
-});
-
-function click_hander(ev) {
-  //console.log('ev', ev);
   var px = ev.pixel;
   var props = disProvince.getDistrictByContainerPos(px);
-  //console.log('props1', props);
+  if (verbose) {
+    console.log("props1", props);
+  }
   if (!props) {
     props = disChongqing.getDistrictByContainerPos(px);
   }
-  //console.log('props1', props);
+  if (verbose) {
+    console.log("props1", props);
+  }
   if (props) {
     const seperator = " ";
     // 京津沪
@@ -116,6 +127,10 @@ function click_hander(ev) {
           status,
           result
         ) {
+          if (verbose) {
+            console.log("status", status);
+            console.log("result", result);
+          }
           if (status == "complete") {
             if (!is_chongqing) {
               for (entry of result.districtList[0].districtList) {
@@ -127,6 +142,9 @@ function click_hander(ev) {
                   text: { content: text },
                   rank: entry.adcode == props.adcode.toString() ? 2 : 1
                 };
+                if (verbose) {
+                  console.log("option", option);
+                }
                 var labelMarker = new AMap.LabelMarker(option);
                 layer.add(labelMarker);
               }
@@ -144,7 +162,9 @@ function click_hander(ev) {
                   },
                   rank: entry.adcode == props.adcode.toString() ? 2 : 1
                 };
-                //console.log("option, 重庆郊县", option);
+                if (verbose) {
+                  console.log("option, 重庆郊县", option);
+                }
                 var labelMarker = new AMap.LabelMarker(option);
                 layer.add(labelMarker);
               }
@@ -161,8 +181,9 @@ function click_hander(ev) {
                 },
                 rank: props.adcode_cit.toString() == "500100" ? 2 : 1
               };
-
-              //console.log("option, 重庆城区", option);
+              if (verbose) {
+                console.log("option, 重庆城区", option);
+              }
               var labelMarker = new AMap.LabelMarker(option);
               layer.add(labelMarker);
             }
@@ -173,4 +194,12 @@ function click_hander(ev) {
   }
 }
 
-//map.on("click", click_hander);
+function initClickHandler() {
+  layer = new AMap.LabelsLayer({
+    fitView: true,
+    map: map
+  });
+  map.on("click", clickHander);
+}
+
+document.addEventListener("DOMContentLoaded", initVirusMap);
