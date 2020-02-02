2cc5db9d20089925dd0cf724fdf45e55d689e484
lispc <mycinbrin@gmail.com>
2020-02-02 04:21:31 +0000

    auto update

--- confirmed_data.js
+++ confirmed_data.js
@@ -1,4 +1,4 @@
-const LAST_UPDATE = "2020.02.02-12:16:25";
+const LAST_UPDATE = "2020.02.02-12:21:31";
 const DATA = {
   "110000": {
     "confirmedCount": 183,
@@ -6,7 +6,7 @@ const DATA = {
     "color": "#B80000"
   },
   "120000": {
-    "confirmedCount": 41,
+    "confirmedCount": 45,
     "cityName": "天津市",
     "color": "#FF5F5F"
   },
