c6a771b3302d94f6143cce8bd9aae329564e84df
lispc <mycinbrin@gmail.com>
2020-02-05 14:31:53 +0000

    auto update

--- confirmed_data.js
+++ confirmed_data.js
@@ -1,4 +1,4 @@
-const LAST_UPDATE = "2020.02.05-22:26:48";
+const LAST_UPDATE = "2020.02.05-22:31:52";
 const DATA = {
   "110000": {
     "confirmedCount": 253,
@@ -1181,7 +1181,7 @@ const DATA = {
     "color": "#FFBFBF"
   },
   "460200": {
-    "confirmedCount": 22,
+    "confirmedCount": 30,
     "cityName": "三亚市",
     "color": "#FF9F9F"
   },
